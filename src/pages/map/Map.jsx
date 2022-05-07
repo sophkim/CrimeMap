825e17b41e806749a5197e36d12206a0f3fe2d8c
sophkim <soph.b.kim@gmail.com>
2022-05-07 17:52:20 +0900

    commit

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,30 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 
-const { kakao } = window;
+const options = {
+  //지도를 생성할 때 필요한 기본 옵션
+  center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+  level: 3, //지도의 레벨(확대, 축소 정도)
+};
 
-const Map = () => {
-  const [map, setMap] = useState(null);
+function Map() {
+  const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
 
-  //처음 지도 그리기
   useEffect(() => {
-    const container = document.getElementById("map");
-    const options = { center: new kakao.maps.LatLng(33.450701, 126.570667) };
-    const kakaoMap = new kakao.maps.Map(container, options);
-    setMap(kakaoMap);
+    new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
+    return () => {};
   }, []);
 
   return (
     <div
-      style={{
-        width: "100%",
-        display: "inline-block",
-        marginLeft: "5px",
-        marginRight: "5px",
-      }}
-    >
-      <div id="map" style={{ width: "99%", height: "500px" }}></div>
-    </div>
+      className="map"
+      style={{ width: "650px", height: "600px" }}
+      ref={container}
+    ></div>
   );
-};
+}
 
 export default Map;
