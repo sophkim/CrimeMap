e1c6ae413bd98d6b1cd9d6feadee025649e2075d
sophkim <soph.b.kim@gmail.com>
2022-05-16 17:00:30 +0900

    change map api

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,48 +1,26 @@
 import React, { useEffect } from "react";
 import "./map.css";
 
-const { kakao } = window;
+//const { kakao } = window;
+const { Tmapv2 } = window;
 
 const Map = () => {
   useEffect(() => {
-    const container = document.getElementById("myMap");
-
-    //지도 중심좌표 및 확대축소 레벨
-    const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
-    };
-
-    //지도 생성
-    const map = new kakao.maps.Map(container, options);
-
-    //마커 생성
-    const marker = new kakao.maps.Marker({
-      position: options.center,
+    const map = new Tmapv2.Map("myMap", {
+      center: new Tmapv2.LatLng(37.566481622437934, 126.98502302169841),
+      httpsMode: true,
+      zoom: 15,
     });
-
-    //지도 위에 마커 표시
-    marker.setMap(map);
-
-    // 지도 타입 컨트롤러 - 일반지도 & 스카이뷰
-    const mapTypeControl = new kakao.maps.MapTypeControl();
-
-    //지도에 컨트롤 추가
-    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-    //확대 축소 줌컨트롤러 추가
-    const zoomControl = new kakao.maps.ZoomControl();
-    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
   }, []);
 
   return (
     <div
       id="myMap"
       style={{
-        width: "650px",
+        width: "900px",
         height: "600px",
         position: "absolute",
-        right: "80px",
+        right: "70px",
         top: "150px",
       }}
     ></div>
