93f5bb0e0c3fdfb33f82ae64cf0310f33be7a167
sophkim <soph.b.kim@gmail.com>
2022-05-16 18:17:18 +0900

    map css

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "./map.css";
 
-//const { kakao } = window;
 const { Tmapv2 } = window;
 
 const Map = () => {
@@ -14,16 +13,40 @@ const Map = () => {
   }, []);
 
   return (
-    <div
-      id="myMap"
-      style={{
-        width: "900px",
-        height: "600px",
-        position: "absolute",
-        right: "70px",
-        top: "150px",
-      }}
-    ></div>
+    <div className="Map">
+      <div className="sidebar">
+        <input
+          className="sidebarStart"
+          type="text"
+          placeholder="출발지를 입력해주세요."
+        ></input>
+        <div className="sidebarEnd"></div>
+        <input
+          className="sidebarStart"
+          type="text"
+          placeholder="도착지를 입력해주세요."
+        ></input>
+        <button className="sidebarBtn">시작하기</button>
+      </div>
+      <div className="sidebar2">
+        <p>
+          안전경로를 추천해 줍니다:)
+          <br />
+          우선 출발지와 도착지를 입력해주세요!
+        </p>
+      </div>
+      <div
+        id="myMap"
+        style={{
+          width: "900px",
+          height: "600px",
+          position: "absolute",
+          right: "70px",
+          top: "150px",
+          textAlign: "right",
+        }}
+      ></div>
+    </div>
   );
 };
 
