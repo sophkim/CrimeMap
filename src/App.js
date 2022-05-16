1384e7ac3e08b59583120e48e0ef7ecd921634c7
sophkim <soph.b.kim@gmail.com>
2022-05-16 18:27:14 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Map from "./pages/map/Map";
+//import Main from "./pages/map/Main";
 import Header from "./components/Header";
+//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import "./home.css";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <b>Main page</b>
+    </div>
+  );
+};
+
+export default Main;
