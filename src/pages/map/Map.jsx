a3e20ded6943261b9ef9614bfeddde17359260a3
sophkim <soph.b.kim@gmail.com>
2022-06-02 12:59:19 +0900

    경로구현

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -13,180 +13,6 @@ const Map = () => {
       zoom: 15,
     });
 
-    let marker_s, marker_e, marker, cctv;
-
-    //시작 마커
-    // marker_s = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.568085523663385, 126.98605733268329),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_s.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    //도착 마커
-    // marker_e = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.56445848334345, 127.00973587385866),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_e.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    //경유지 6개
-    // marker = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.56626352138058, 126.98735015742581),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_1.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    // marker = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.56568310756034, 127.00221495976581),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_2.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    // marker = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.570369, 126.992153),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_3.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    // marker = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.56335290252303, 127.00352387777271),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_4.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    // marker = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.570721714117965, 127.00186090818215),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_5.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    // marker = new Tmapv2.Marker({
-    //   position: new Tmapv2.LatLng(37.56515390827723, 126.99066536776698),
-    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_6.png",
-    //   iconSize: new Tmapv2.Size(24, 38),
-    //   map: map,
-    // });
-
-    //경로 선 그리기
-    // let headers = {};
-    // headers["appKey"] = "l7xxf4d6ce3985d1419b9a268be498b40d48";
-
-    // $.ajax({
-    //   type: "POST",
-    //   headers: headers,
-    //   url: "https://apis.openapi.sk.com/tmap/routes/routeOptimization10?version=1&format=json", //
-    //   async: false,
-    //   contentType: "application/json",
-    //   data: JSON.stringify({
-    //     reqCoordType: "WGS84GEO",
-    //     resCoordType: "EPSG3857",
-    //     startName: "출발",
-    //     //startX: "126.98605733268329",
-    //     //startY: "37.568085523663385",
-    //     startTime: "201711121314",
-    //     endName: "도착",
-    //     endX: "127.00973587385866",
-    //     endY: "37.56445848334345",
-    //     searchOption: "0",
-    //     viaPoints: [
-    //       // {
-    //       //   viaPointId: "test01",
-    //       //   viaPointName: "test01",
-    //       //   viaX: "126.98735015742581",
-    //       //   viaY: "37.56626352138058",
-    //       // },
-    //       {
-    //         viaPointId: "test02",
-    //         viaPointName: "test02",
-    //         viaX: "127.00221495976581",
-    //         viaY: "37.56568310756034",
-    //       },
-    //       {
-    //         viaPointId: "test03",
-    //         viaPointName: "test03",
-    //         viaX: "126.992153",
-    //         viaY: "37.570369",
-    //       },
-    //       {
-    //         viaPointId: "test04",
-    //         viaPointName: "test04",
-    //         viaX: "127.00352387777271",
-    //         viaY: "37.56335290252303",
-    //       },
-    //       {
-    //         viaPointId: "test05",
-    //         viaPointName: "test05",
-    //         viaX: "127.00186090818215",
-    //         viaY: "37.570721714117965",
-    //       },
-    //       {
-    //         viaPointId: "test06",
-    //         viaPointName: "test06",
-    //         viaX: "126.99066536776698",
-    //         viaY: "37.56515390827723",
-    //       },
-    //     ],
-    //   }),
-    //   success: function (response) {
-    //     var resultData = response.properties;
-    //     var resultFeatures = response.features;
-
-    //     for (var i in resultFeatures) {
-    //       var geometry = resultFeatures[i].geometry;
-    //       var properties = resultFeatures[i].properties;
-    //       var polyline_;
-
-    //       let drawInfoArr = [];
-
-    //       if (geometry.type == "LineString") {
-    //         for (var j in geometry.coordinates) {
-    //           // 경로들의 결과값(구간)들을 포인트 객체로 변환
-    //           var latlng = new Tmapv2.Point(
-    //             geometry.coordinates[j][0],
-    //             geometry.coordinates[j][1]
-    //           );
-    //           // 포인트 객체를 받아 좌표값으로 변환
-    //           var convertPoint =
-    //             new Tmapv2.Projection.convertEPSG3857ToWGS84GEO(latlng);
-    //           // 포인트객체의 정보로 좌표값 변환 객체로 저장
-    //           var convertChange = new Tmapv2.LatLng(
-    //             convertPoint._lat,
-    //             convertPoint._lng
-    //           );
-
-    //           drawInfoArr.push(convertChange);
-    //         }
-
-    //         polyline_ = new Tmapv2.Polyline({
-    //           path: drawInfoArr,
-    //           strokeColor: "#FF0000",
-    //           strokeWeight: 6,
-    //           map: map,
-    //         });
-    //       }
-    //     }
-    //   },
-    //   error: function (request, status, error) {
-    //     console.log(
-    //       "code:" +
-    //         request.status +
-    //         "\n" +
-    //         "message:" +
-    //         request.responseText +
-    //         "\n" +
-    //         "error:" +
-    //         error
-    //     );
-    //   },
-    // });
-
     // 출발지 설정 - POI 통합 검색 API 요청
     var markerArr = [];
 
@@ -293,7 +119,7 @@ const Map = () => {
       } //startPoint
     );
 
-    // 출발지 설정 - POI 통합 검색 API 요청
+    // 도착지 설정 - POI 통합 검색 API 요청
     var markerArr = [];
 
     $("#endPoint").keydown(
@@ -365,7 +191,7 @@ const Map = () => {
               map.panToBounds(positionBounds);
               map.zoomOut();
 
-              // marker 클릭이벤트 - 출발지 설정 & 기존 마커 및 poi 리스트 삭제
+              // marker 클릭이벤트 - 도착지 설정 & 기존 마커 및 poi 리스트 삭제
               marker.addListener("click", function (e) {
                 for (var i in markerArr) {
                   markerArr[i].setMap(null);
@@ -398,6 +224,183 @@ const Map = () => {
         } //if
       } //startPoint
     );
+
+    let marker_s, marker_e, marker;
+
+    //시작 마커
+    // marker_s = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.568085523663385, 126.98605733268329),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_s.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    //도착 마커
+    // marker_e = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.56445848334345, 127.00973587385866),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_e.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    //경유지 6개
+    // marker = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.56626352138058, 126.98735015742581),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_1.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    // marker = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.56568310756034, 127.00221495976581),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_2.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    // marker = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.570369, 126.992153),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_3.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    // marker = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.56335290252303, 127.00352387777271),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_4.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    // marker = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.570721714117965, 127.00186090818215),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_5.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    // marker = new Tmapv2.Marker({
+    //   position: new Tmapv2.LatLng(37.56515390827723, 126.99066536776698),
+    //   icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_6.png",
+    //   iconSize: new Tmapv2.Size(24, 38),
+    //   map: map,
+    // });
+
+    //경로 선 그리기 (버튼 클릭 이벤트로 구현)
+    // let headers = {};
+    // headers["appKey"] = "l7xxf4d6ce3985d1419b9a268be498b40d48";
+
+    // $("#btn_select").click(function (key) {
+    //   $.ajax({
+    //     type: "POST",
+    //     headers: headers,
+    //     url: "https://apis.openapi.sk.com/tmap/routes/routeOptimization10?version=1&format=json", //
+    //     async: false,
+    //     contentType: "application/json",
+    //     data: JSON.stringify({
+    //       reqCoordType: "WGS84GEO",
+    //       resCoordType: "EPSG3857",
+    //startX: markerPosition,
+    //startY: "37.568085523663385",
+    //startTime: "201711121314",
+    //endName: "도착",
+    //endX: "127.00973587385866",
+    //endY: "37.56445848334345",
+    //searchOption: "0",
+
+    // 경유지
+    // viaPoints: [
+    //   {
+    //     viaPointId: "test01",
+    //     viaPointName: "test01",
+    //     viaX: "126.98735015742581",
+    //     viaY: "37.56626352138058",
+    //   },
+    //   {
+    //     viaPointId: "test02",
+    //     viaPointName: "test02",
+    //     viaX: "127.00221495976581",
+    //     viaY: "37.56568310756034",
+    //   },
+    //   {
+    //     viaPointId: "test03",
+    //     viaPointName: "test03",
+    //     viaX: "126.992153",
+    //     viaY: "37.570369",
+    //   },
+    //   {
+    //     viaPointId: "test04",
+    //     viaPointName: "test04",
+    //     viaX: "127.00352387777271",
+    //     viaY: "37.56335290252303",
+    //   },
+    //   {
+    //     viaPointId: "test05",
+    //     viaPointName: "test05",
+    //     viaX: "127.00186090818215",
+    //     viaY: "37.570721714117965",
+    //   },
+    //   {
+    //     viaPointId: "test06",
+    //     viaPointName: "test06",
+    //     viaX: "126.99066536776698",
+    //     viaY: "37.56515390827723",
+    //   },
+    //],
+    // }),
+    // success: function (response) {
+    //   var resultData = response.properties;
+    //   var resultFeatures = response.features;
+
+    //   for (var i in resultFeatures) {
+    //     var geometry = resultFeatures[i].geometry;
+    //     var properties = resultFeatures[i].properties;
+    //     var polyline_;
+
+    //     let drawInfoArr = [];
+
+    //     if (geometry.type === "LineString") {
+    //       for (var j in geometry.coordinates) {
+    // 경로들의 결과값(구간)들을 포인트 객체로 변환
+    // var latlng = new Tmapv2.Point(
+    //   geometry.coordinates[j][0],
+    //   geometry.coordinates[j][1]
+    // );
+    // 포인트 객체를 받아 좌표값으로 변환
+    // var convertPoint =
+    //   new Tmapv2.Projection.convertEPSG3857ToWGS84GEO(latlng);
+    // 포인트객체의 정보로 좌표값 변환 객체로 저장
+    // var convertChange = new Tmapv2.LatLng(
+    //   convertPoint._lat,
+    //   convertPoint._lng
+    // );
+
+    //             drawInfoArr.push(convertChange);
+    //           }
+
+    //           polyline_ = new Tmapv2.Polyline({
+    //             path: drawInfoArr,
+    //             strokeColor: "#FF0000",
+    //             strokeWeight: 6,
+    //             map: map,
+    //           });
+    //         }
+    //       }
+    //     },
+    //     error: function (request, status, error) {
+    //       console.log(
+    //         "code:" +
+    //           request.status +
+    //           "\n" +
+    //           "message:" +
+    //           request.responseText +
+    //           "\n" +
+    //           "error:" +
+    //           error
+    //       );
+    //     },
+    //   });
+    // });
   }, []);
 
   return (
